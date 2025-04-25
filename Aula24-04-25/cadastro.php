2167f518ead1f834f1e98e1825649af84e632106
Enzo Darcy <=>
2025-04-24 21:03:54 -0300

    Adicionei uma pasta pra testes e enviei o exercício da Aula 24 4 25

--- Aula24-04-25/cadastro.php
+++ Aula24-04-25/cadastro.php
@@ -8,22 +8,20 @@
 </head>
 <body>
     <form action="" method="get">
-        <input name="produto" type="text" placeholder="produto"><br>
+        <input name="nome" type="text" placeholder="Produto"><br>
         <select name="categoria" id="">
             <option value="limpeza">Limpeza</option>
             <option value="cereais">Cereais</option>
             <option value="armarinho">Armarinho</option>
         </select><br>
-        <input name="fabricante" type="text" placeholder="fabricante"><br>
+        <input name="fabricante" type="text" placeholder="Fabricante"><br>
         <button type="submit">Enviar</button>
     </form>
     <?php
         session_start();
-        if (!empty($_GET["produto"]) && !empty($_GET["categoria"]) && !empty($_GET["fabricante"])) {
-            $_SESSION["produtos"] = [$_GET["produto"], $_GET["categoria"], $_GET["fabricante"]];
-        }
-        else {
-            echo "teste'nt";
+        if (!empty($_GET["nome"]) && !empty($_GET["categoria"]) && !empty($_GET["fabricante"])) {
+            $produto = [$_GET["nome"], $_GET["categoria"], $_GET["fabricante"]];
+            $_SESSION["produtos"][count($_SESSION["produtos"])] = $produto;
         }
     ?>
 </body>
--- Aula24-04-25/listar.php
+++ Aula24-04-25/listar.php
@@ -5,14 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table {
+            font-family: 'Courier New', Courier, monospace;
+            border-collapse: collapse;
+        }
+        td {
+            border: 1px solid black;
+        }
+        #negrito {
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
     <?php
         session_start();
-        foreach ($_SESSION["produtos"] as $element => $value) {
-            echo $element . "   ---   ";
+        echo '<table>';
+        echo '<tr id="negrito"><td>NOME</td><td>CATEGORIA</td><td>FABRICANTE</td></tr>';
+        for ($i = 0; $i < count($_SESSION["produtos"]); $i++) {
+            echo "<tr>";
+            for ($j = 0; $j < count($_SESSION["produtos"][$i]); $j++) {
+                echo "<td>";
+                echo $_SESSION["produtos"][$i][$j];
+                echo "</td>";
+            }
+            echo "</tr>";
         }
-        echo "<br> --> " . $_SESSION["produtos"][1];
+        echo "</table>";
     ?>
 </body>
 </html>
